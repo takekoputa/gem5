e49017eb7fb8d62a6e8c8302bc34e4a15754a484
Hoa Nguyen <hoanguyen@ucdavis.edu>
2021-06-06 22:38:15 -0700

    ext: Add the barebone of SST integration
    Change-Id: I5956261f025701b0e015b9347d0cb7cc67239282
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

---
+++ ext/sst2/gem5.hh
@@ -0,0 +1,58 @@
+#ifndef __GEM5_COMPONENT_H__
+#define __GEM5_COMPONENT_H__
+
+#include <string>
+#include <vector>
+
+#include <sst/core/sst_config.h>
+#include <sst/core/component.h>
+
+#include <sst/core/simulation.h>
+#include <sst/core/interfaces/stringEvent.h>
+#include <sst/core/interfaces/simpleMem.h>
+
+#include <sim/simulate.hh>
+
+#include <sst/core/eli/elementinfo.h>
+#include <sst/core/link.h>
+
+
+class gem5Component: public SST::Component
+{
+  public:
+    gem5Component(SST::ComponentId_t id, SST::Params& params);
+    ~gem5Component();
+
+    void setup();
+    void finish();
+    bool clockTick(SST::Cycle_t currentCycle);
+
+  private:
+    SST::Output output;
+
+    void initPython(int argc, char **argv);
+    void splitCommandArgs(std::string &cmd, std::vector<char*> &args);
+
+
+  public: // register the component to SST
+    SST_ELI_REGISTER_COMPONENT(
+        gem5Component,
+        "gem5", // SST will look for libgem5.so
+        "gem5Component",
+        SST_ELI_ELEMENT_VERSION(1, 0, 0),
+        "Initialize gem5 and link SST's ports to gem5's ports",
+        COMPONENT_CATEGORY_UNCATEGORIZED
+    )
+
+    SST_ELI_DOCUMENT_PARAMS(
+        {"cmd", "command to run gem5's config"}
+    )
+
+    //SST_ELI_DOCUMENT_PORTS(
+    //    {"icache_port", "port to gem5's icache", {"memHierarchy.MemEventBase"}},
+    //    {"dcache_port", "port to gem5's dcache", {"memHierarchy.MemEventBase"}}
+    //)
+
+};
+
+#endif // __GEM5_COMPONENT_H__
---
+++ ext/sst2/sst/simple.py
@@ -0,0 +1,89 @@
+import sst
+import sys
+import os
+
+cache_link_latency = "1ns"
+
+l1_params = {
+    "access_latency_cycles" : "1",
+    "cache_frequency" : "2 Ghz",
+    "replacement_policy" : "lru",
+    "coherence_protocol" : "MESI",
+    "associativity" : "4",
+    "cache_line_size" : "64",
+    "cache_size" : "4 KB",
+    "L1" : "1",
+    "debug" : "0",
+}
+
+l2_params = {
+    "access_latency_cycles" : "8",
+    "cache_frequency" : "2 Ghz",
+    "replacement_policy" : "lru",
+    "coherence_protocol" : "MESI",
+    "associativity" : "8",
+    "cache_line_size" : "64",
+    "cache_size" : "32 KB",
+    "debug" : "0",
+}
+
+cpu_params = {
+    "frequency": "1GHz",
+    "cmd": "gem5/cpu.py --cpu-type TimingSimpleCPU --not-simulate"
+}
+
+gem5_node = sst.Component("node", "gem5.gem5Component")
+gem5_node.addParams(cpu_params)
+
+"""
+# L1 icache
+l1i_cache = sst.Component("l1i_cache", "memHierarchy.Cache")
+l1i_cache.addParams(l1_params)
+# L1 dcache
+l1d_cache = sst.Component("l1d_cache", "memHierarchy.Cache")
+l1d_cache.addParams(l1_params)
+
+# TODO: shared L2Cache
+
+# Memory
+memctrl = sst.Component("memory", "memHierarchy.MemController")
+memctrl.addParams({
+    "debug" : "0",
+    "clock" : "1GHz",
+    "request_width" : "64",
+    "addr_range_end" : 1024*1024*1024-1,
+})
+memory = memctrl.setSubComponent("backend", "memHierarchy.simpleMem")
+memory.addParams({
+    "access_time" : "30ns",
+    "mem_size" : "1GiB",
+})
+
+
+
+# Connections
+# cpu <-> L1
+cpu_icache_link = sst.Link("cpu_l1i_cache_link")
+cpu_icache_link.connect(
+    (gem5_node, "icache_port", cache_link_latency),
+    (l1i_cache, "high_network_0", cache_link_latency)
+)
+
+cpu_dcache_link = sst.Link("cpu_l1d_cache_link")
+cpu_dcache_link.connect(
+    (gem5_node, "dcache_port", cache_link_latency),
+    (l1d_cache, "high_network_0", cache_link_latency)
+)
+
+# L1 <-> mem
+icache_mem_link = sst.Link("l1i_cache_mem_link")
+icache_mem_link.connect(
+    (l1i_cache, "low_network_0", cache_link_latency),
+    (memctrl, "direct_link", cache_link_latency)
+)
+dcache_mem_link = sst.Link("l1d_cache_mem_link")
+dcache_mem_link.connect(
+    (l1d_cache, "low_network_0", cache_link_latency),
+    (memctrl, "direct_link", cache_link_latency)
+)
+"""
