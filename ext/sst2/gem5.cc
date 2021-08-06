81cd9d4e4cd614aa58e3aecdd882dda054f1f34b
Hoa Nguyen <hoanguyen@ucdavis.edu>
2021-08-06 15:57:14 -0700

    ext: rebase to gem5/dev-for-v21.2
    Change-Id: I6789dbee4ce8fa06de8cf7bfbf64f71510122570
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

---
+++ ext/sst2/gem5.cc
@@ -0,0 +1,313 @@
+#include <sst/core/sst_config.h>
+#include <sst/core/componentInfo.h>
+#include <Python.h>  // Before serialization to prevent spurious warnings
+
+#include "gem5.hh"
+
+// System headers
+#include <string>
+#include <vector>
+
+// gem5 Headers
+#include <sim/core.hh>
+#include <sim/init.hh>
+#include <sim/init_signals.hh>
+#include <sim/system.hh>
+#include <sim/sim_events.hh>
+#include <sim/sim_object.hh>
+#include <base/logging.hh>
+#include <base/debug.hh>
+
+#include <cassert>
+
+//#ifdef fatal  // gem5 sets this
+//#undef fatal
+//#endif
+
+// More SST Headers
+#include <core/timeConverter.h>
+
+gem5Component::gem5Component(SST::ComponentId_t id, SST::Params& params):
+    SST::Component(id)
+{
+    output.init("gem5Component-" + getName() + "->", 1, 0, SST::Output::STDOUT);
+
+    // Register a handler to be called on a set frequency.
+    SST::TimeConverter *clock = registerClock(
+        "1MHz",
+        new SST::Clock::Handler<gem5Component>(this, &gem5Component::clockTick)
+    );
+
+    // how many gem5 cycles will be simulated within an SST clock tick
+    gem5_sim_cycles = clock->getFactor();
+
+    // "cmd" -> gem5's Python
+    std::string cmd = params.find<std::string>("cmd", "");
+    if (cmd.empty()) {
+        output.output(
+            CALL_INFO, "Component %s must have a 'cmd' parameter.\n",
+            getName().c_str()
+        );
+        exit(-1);
+    }
+
+    // Telling SST the command line call to gem5
+    std::vector<char*> args;
+    args.push_back(const_cast<char*>("sst.x"));
+    splitCommandArgs(cmd, args);
+    output.output(CALL_INFO, "Command string:  [sst.x %s]\n", cmd.c_str());
+    for (size_t i = 0; i < args.size(); ++i) {
+        output.output(CALL_INFO, "  Arg [%02zu] = %s\n", i, args[i]);
+    }
+
+    initPython(args.size(), &args[0]);
+
+    registerAsPrimaryComponent();
+    primaryComponentDoNotEndSim();
+}
+
+gem5Component::~gem5Component()
+{
+}
+
+void
+gem5Component::init(unsigned phase)
+{
+    output.output(CALL_INFO," init phase: %u\n", phase);
+    if (phase == 0) {
+        const std::vector<std::string> instantiate_command_2 = {
+            "m5.instantiate_step_2()"
+        };
+        this->execPythonCommands(instantiate_command_2);
+    }
+
+    /*
+    const std::vector<std::string> find_sim_object_commands = {
+        "print('-----------------------------------------')",
+        "from m5.objects import OutgoingRequestBridge, Root",
+        "root = Root.getInstance()",
+        "print(root.find_all(OutgoingRequestBridge))",
+        "for obj in root.descendants(): print(type(obj))"
+    };
+    this->execPythonCommands(find_sim_object_commands);
+    */
+    SSTResponder* system_port = loadUserSubComponent<SSTResponder>("system_port", 0);
+    gem5_connectors.push_back(system_port);
+    SSTResponder* cache_port = loadUserSubComponent<SSTResponder>("cache_port", 0);
+    gem5_connectors.push_back(cache_port);
+    //SSTResponder* icache_port = loadUserSubComponent<SSTResponder>("icache_port", 0);
+    //gem5_connectors.push_back(icache_port);
+    //SSTResponder* dcache_port = loadUserSubComponent<SSTResponder>("dcache_port", 0);
+    //gem5_connectors.push_back(dcache_port);
+}
+
+void
+gem5Component::setup()
+{
+    output.verbose(CALL_INFO, 1, 0, "Component is being setup.\n");
+}
+
+void
+gem5Component::finish()
+{
+    output.verbose(CALL_INFO, 1, 0, "Component is being finished.\n");
+}
+
+bool
+gem5Component::clockTick(SST::Cycle_t currentCycle)
+{
+    // what to do in a SST's Tick
+
+    /*
+    for (auto requestor: requestors)
+        requestor->clock();
+    */
+
+//    return true;
+
+    gem5::GlobalSimLoopExitEvent *event = gem5::simulate(gem5_sim_cycles);
+    clocks_processed++;
+    if (event != gem5::simulate_limit_event) { // gem5 exits due to reasons other than reaching simulation limit
+        output.output("exiting: curTick()=%lu cause=`%s` code=%d\n",
+            gem5::curTick(), event->getCause().c_str(), event->getCode()
+        );
+        primaryComponentOKToEndSim();
+        return true;
+    }
+
+    // returning False means the simulation should go on
+    return false;
+
+}
+
+#define PyCC(x) (const_cast<char *>(x))
+
+int
+gem5Component::execPythonCommands(const std::vector<std::string>& commands)
+{
+    PyObject *dict = PyModule_GetDict(this->python_main);
+
+    // import the main m5 module
+    PyObject *result;
+
+    for (auto const command: commands) {
+        result = PyRun_String(command.c_str(), Py_file_input, dict, dict);
+        if (!result) {
+            PyErr_Print();
+            return 1;
+        }
+        Py_DECREF(result);
+    }
+}
+
+int
+gem5Component::startM5(int argc, char **_argv)
+{
+#if HAVE_PROTOBUF
+    // Verify that the version of the protobuf library that we linked
+    // against is compatible with the version of the headers we
+    // compiled against.
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+#endif
+
+
+#if PY_MAJOR_VERSION >= 3
+    typedef std::unique_ptr<wchar_t[], decltype(&PyMem_RawFree)> WArgUPtr;
+    std::vector<WArgUPtr> v_argv;
+    std::vector<wchar_t *> vp_argv;
+    v_argv.reserve(argc);
+    vp_argv.reserve(argc);
+    for (int i = 0; i < argc; i++) {
+        v_argv.emplace_back(Py_DecodeLocale(_argv[i], NULL), &PyMem_RawFree);
+        vp_argv.emplace_back(v_argv.back().get());
+    }
+
+    wchar_t **argv = vp_argv.data();
+#else
+    char **argv = _argv;
+#endif
+
+    PySys_SetArgv(argc, argv);
+
+    // We have to set things up in the special __main__ module
+    //PyObject *module = PyImport_AddModule(PyCC("__main__"));
+    python_main = PyImport_AddModule(PyCC("__main__"));
+    if (python_main == NULL)
+        panic("Could not import __main__");
+
+    const std::vector<std::string> commands = {
+        "import m5",
+        "m5.main()"
+    };
+    this->execPythonCommands(commands);
+
+#if HAVE_PROTOBUF
+    google::protobuf::ShutdownProtobufLibrary();
+#endif
+
+    return 0;
+}
+
+void
+gem5Component::initPython(int argc, char *_argv[])
+{
+    // should be similar to gem5's src/sim/main.cc
+    PyObject *mainModule, *mainDict;
+
+    int ret;
+
+    // Initialize m5 special signal handling.
+    gem5::initSignals();
+
+#if PY_MAJOR_VERSION >= 3
+    std::unique_ptr<wchar_t[], decltype(&PyMem_RawFree)> program(
+        Py_DecodeLocale(_argv[0], NULL),
+        &PyMem_RawFree);
+    Py_SetProgramName(program.get());
+#else
+    Py_SetProgramName(_argv[0]);
+#endif
+
+    // Register native modules with Python's init system before
+    // initializing the interpreter.
+    if (!Py_IsInitialized()) {
+        gem5::registerNativeModules();
+        // initialize embedded Python interpreter
+        Py_Initialize();
+    }
+    else {
+        // https://stackoverflow.com/a/28349174
+        PyImport_AddModule("_m5");
+        PyObject* module = gem5::EmbeddedPyBind::initAll();
+        PyObject* sys_modules = PyImport_GetModuleDict();
+        PyDict_SetItemString(sys_modules, "_m5", module);
+        Py_DECREF(module);
+    }
+
+
+    // Initialize the embedded m5 python library
+    ret = gem5::EmbeddedPython::initAll();
+
+    if (ret == 0)
+    {
+        // start m5
+        this->startM5(argc, _argv);
+    }
+    else
+    {
+        output.output(CALL_INFO, "Not calling m5Main due to ret=%d\n", ret);
+    }
+
+    const std::vector<std::string> instantiate_command_1 = {
+        "m5.instantiate_step_1()"
+    };
+    this->execPythonCommands(instantiate_command_1);
+}
+
+void
+gem5Component::splitCommandArgs(std::string &cmd, std::vector<char*> &args)
+{
+    const std::array<char, 4> delimiters = { {'\\', ' ', '\'', '\"'} };
+
+    std::vector<std::string> parsed_args1;
+    std::vector<std::string> parsed_args2;
+
+    auto prev = &(parsed_args1);
+    auto curr = &(parsed_args2);
+
+    curr->push_back(cmd);
+
+    for (auto delimiter: delimiters)
+    {
+        std::swap(prev, curr);
+        curr->clear();
+
+        for (auto part: *prev)
+        {
+            size_t left = 0;
+            size_t right = 0;
+            size_t part_length = part.size();
+
+            while (left < part_length)
+            {
+                while ((left < part_length) && (part[left] == delimiter))
+                    left++;
+                
+                if (!(left < part_length))
+                    break;
+
+                right = part.find(delimiter, left);
+                if (right == part.npos)
+                    right = part_length;
+
+                if (left <= right)
+                    curr->push_back(part.substr(left, right-left));
+                
+                left = right + 1;
+            }
+        }
+    }
+
+    for (auto part: *curr)
+        args.push_back(strdup(part.c_str()));
+}
---
+++ ext/sst2/gem5/riscv_fs.py
@@ -0,0 +1,126 @@
+import m5
+from m5.objects import *
+from os import path
+
+ExternalResponder = ExternalSlave
+
+def generateMemNode(state, mem_range):
+    node = FdtNode("memory@%x" % int(mem_range.start))
+    node.append(FdtPropertyStrings("device_type", ["memory"]))
+    node.append(FdtPropertyWords("reg",
+        state.addrCells(mem_range.start) +
+        state.sizeCells(mem_range.size()) ))
+    return node
+
+def generateDtb(system):
+    """
+    Autogenerate DTB. Arguments are the folder where the DTB
+    will be stored, and the name of the DTB file.
+    """
+    state = FdtState(addr_cells=2, size_cells=2, cpu_cells=1)
+    root = FdtNode('/')
+    root.append(state.addrCellsProperty())
+    root.append(state.sizeCellsProperty())
+    root.appendCompatible(["riscv-virtio"])
+
+    for mem_range in system.mem_ranges:
+        root.append(generateMemNode(state, mem_range))
+
+    sections = [*system.cpu, system.platform]
+
+    for section in sections:
+        for node in section.generateDeviceTree(state):
+            if node.get_name() == root.get_name():
+                root.merge(node)
+            else:
+                root.append(node)
+
+    fdt = Fdt()
+    fdt.add_rootnode(root)
+    fdt.writeDtsFile(path.join(m5.options.outdir, 'device.dts'))
+    fdt.writeDtbFile(path.join(m5.options.outdir, 'device.dtb'))
+
+def createHiFivePlatform(system):
+    system.internal_membus = SystemXBar()
+    system.internal_membus.badaddr_responder = BadAddr()
+    system.internal_membus.default = \
+        system.internal_membus.badaddr_responder.pio
+
+    for cpu in system.cpu:
+        """
+        cpu.createThreads()
+        system.cpu_xbar = SystemXBar()
+        system.icache_outgoing_bridge = OutgoingRequestBridge(connector_name = "icache_port") # connector_name = x -> an SST object named "icache_port" will look for this bridge
+        cpu.icache_port = system.icache_outgoing_bridge.port
+        cpu.dcache_port = system.cpu_xbar.cpu_side_ports
+        cpu.mmu.connectWalkerPorts(
+            system.internal_membus.cpu_side_ports, system.internal_membus.cpu_side_ports)
+        system.dcache_outgoing_bridge = OutgoingRequestBridge(connector_name = "dcache_port")
+        system.cpu_xbar.mem_side_ports = system.dcache_outgoing_bridge.port
+        system.cpu_xbar.mem_side_ports = system.internal_membus.cpu_side_ports
+        """
+        cpu.createThreads()
+        system.cpu_xbar = SystemXBar()
+        system.cache_xbar = SystemXBar()
+        system.cache_outgoing_bridge = OutgoingRequestBridge() # connector_name = x -> an SST object named "cache_port" will look for this bridge
+        cpu.icache_port = system.cache_xbar.cpu_side_ports
+        cpu.dcache_port = system.cpu_xbar.cpu_side_ports
+        cpu.mmu.connectWalkerPorts(
+            system.internal_membus.cpu_side_ports, system.internal_membus.cpu_side_ports)
+        system.cpu_xbar.mem_side_ports = system.cache_xbar.cpu_side_ports
+        system.cpu_xbar.mem_side_ports = system.internal_membus.cpu_side_ports
+        system.cache_xbar.mem_side_ports = system.cache_outgoing_bridge.port
+
+    system.platform = HiFive()
+
+    system.platform.rtc = RiscvRTC(frequency=Frequency("100MHz"))
+    system.platform.clint.int_pin = system.platform.rtc.int_pin
+    system.platform.attachPlic()
+
+    system.pma_checker = PMAChecker(
+        uncacheable=[
+            *system.platform._on_chip_ranges(),
+            *system.platform._off_chip_ranges(),
+        ]
+    )
+
+    system.platform.attachOnChipIO(system.internal_membus)
+    system.platform.attachOffChipIO(system.internal_membus)
+
+    print("Done")
+
+
+
+system = System()
+
+system.clk_domain = SrcClockDomain(
+    clock="3GHz", voltage_domain=VoltageDomain()
+)
+
+system.mem_ranges = [AddrRange(start=0x80000000, size="512MB")]
+
+system.cpu = [TimingSimpleCPU(cpu_id=i) for i in range(1)]
+system.mem_mode = 'timing'
+
+#system.intrctrl = IntrControl()
+
+createHiFivePlatform(system)
+
+system.system_outgoing_bridge = OutgoingRequestBridge()
+system.system_port = system.system_outgoing_bridge.port
+
+generateDtb(system)
+system.workload.dtb_filename = path.join(m5.options.outdir, 'device.dtb')
+system.workload.dtb_addr = 0x87e00000 # how to determine this?
+kernel_cmd = [
+    "console=ttyS0",
+    "root=/dev/vda",
+    "ro"
+]
+system.workload.command_line = " ".join(kernel_cmd)
+
+for cpu in system.cpu:
+    cpu.createInterruptController()
+
+root = Root(full_system = True, system = system)
+
