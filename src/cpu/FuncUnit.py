3e4035dacf23153afe8854cd4a65bc0a462db77a
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-08-20 03:03:17 -0700

    arch-riscv: Add Whole register move insts
    Note that those insts are vl- and vtype-agnostic.
    
    Change-Id: I90e4a20db3a338619cb1aa7a61afc8dc64871e85
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- src/cpu/FuncUnit.py
+++ src/cpu/FuncUnit.py
@@ -130,6 +130,8 @@ class OpClass(Enum):
         'VectorReductionMicroOp',
         'VectorVMUNARY0MacroOp',
         'VectorVMUNARY0MicroOp',
+        'VectorWholeRegisterMoveMacroOp',
+        'VectorWholeRegisterMoveMicroOp',
         'VectorUnitStrideMemLoadMacroOp',
         'VectorUnitStrideMemLoadMicroOp'
     ]
